fde9a1e97bfce0df15f4427e89ca9e7c6cb3e789
Sean Murakami <murakami.sean@yahoo.com>
2020-04-10 16:49:37 -0700

    feat: setup sass styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 import { TodoItem } from "./interfaces";
-import "./App.css";
+import "../src/scss/main.scss";
 
 function App() {
   const [todos, setTodos] = useState<TodoItem[]>([]);
