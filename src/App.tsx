9257a8f98c6d7a443ee9b207a8f51cd631f4e645
Sean Murakami <murakami.sean@yahoo.com>
2021-01-28 20:25:38 -0800

    added persistent todos using local storage hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import React, { useState } from "react";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 import { TodoItem } from "./interfaces";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 import "../src/scss/main.scss";
 
 function App() {
-  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [todos, setTodos] = useLocalStorage("todos", []);
   const [count, setCount] = useState(0);
   const addTodo = (todo: TodoItem): void => {
     setTodos([...todos, todo]);
     setCount(count + 1);
   };
   const toggleCompleted = (id: number) => {
-    const newTodos = todos.map(todo => {
+    const newTodos = todos.map((todo: any) => {
       return todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo;
     });
     setTodos(newTodos);
---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export const useLocalStorage = (key: string, initialValue: any) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (err) {
+      console.error(err);
+      return initialValue;
+    }
+  });
+  const setValue = (val: any) => {
+    try {
+      const valueToStore = val instanceof Function ? val(storedValue) : val;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return [storedValue, setValue];
+};
